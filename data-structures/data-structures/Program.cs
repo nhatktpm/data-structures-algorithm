1bd3d490b76dcfd301548388e712c67cbf63f049
Nhat.Nguyen <nhat.nguyen@ptnglobalcorp.com>
2023-03-22 23:48:12 +0700

    git init

---
+++ data-structures/data-structures/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace data_structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ data-structures/data-structures/array/DynamicArray.cs
@@ -0,0 +1,101 @@
+﻿
+
+using System.Collections;
+
+namespace data_structures.array
+{
+    public class DynamicArray<T> : IEnumerable<T>
+    {
+        private T[] arr;
+        private int capacity;
+        private int size = 0;
+
+        public DynamicArray()
+        {
+            this.capacity = 10;
+            arr = new T[capacity];
+        }
+
+        public DynamicArray(int capacity)
+        {
+            if (capacity < 0) throw new ArgumentOutOfRangeException("Capacity cannot be negative" + capacity);
+            this.capacity = capacity;
+            arr = new T[capacity];
+        }
+
+        public bool isEmty()
+        {
+            return this.size == 0;
+        }
+
+        public int Size()
+        {
+            return this.size;
+        }
+
+        public T Get(int idex)
+        {
+            return arr[idex];
+        }
+
+        public void Set(int idex, T emlement)
+        {
+            arr[idex] = emlement;
+        }
+
+        public void Clear()
+        {
+            for (var i = 0; i < size; i++)
+            {
+                arr[i] = default(T);
+            }
+            size = 0;
+
+        }
+
+        public void Add(T element)
+        {
+            if (size >= capacity - 1)
+            {
+                if (capacity == 0) capacity = 1;
+                else
+                {
+                    capacity *= 2;
+                }
+                T[] newArr = new T[capacity];
+                for(int i = 0;i<  arr.Length; i++)
+                {
+                    newArr[i] = arr[i];
+                }
+                arr= newArr;
+            }
+            arr[size++] = element;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
